cd041aa6053b5da920da5513e175529a76160f29
shankarlal <shankarlalsiyag1996@gmail.com>
2026-05-20 12:59:51 +0530

    Config changes for video recording

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,48 +1,33 @@
-// @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
-
-
-const config = {
+export default defineConfig({
   testDir: './tests',
   timeout: 30_000,
   expect: {
-    timeout: 30_000
+    timeout: 30_000,
   },
-  reporter: process.env.CI ? 'bob' : 'html',
+
+  reporter: process.env.CI ? 'blob' : 'html',
+
+  retries: process.env.CI ? 1 : 0,     // retries only in CI
+  workers: process.env.CI ? 2 : 4,
+
   use: {
     baseURL: 'https://login.salesforce.com',
     headless: false,
 
+
     screenshot: 'on',
     video: 'on',
     trace: 'on',
-    storageState: 'auth.json',
+
+    storageState: process.env.CI ? undefined : 'auth.json',
   },
 
-  retries: 1,
-
-  workers: process.env.CI ? 2 : 4,
-  //forbidOnly: !!process.env.CI,
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] }
-    }
-  ]
-
-}
-module.exports = config;
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
-
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
